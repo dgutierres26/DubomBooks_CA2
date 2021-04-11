993e348634de64cd4ae42d0cadc789eab5a05a92
dgutierres26 <diegogutierres@hotmail.com>
2021-04-11 16:25:58 +0000

    Setting Item-controller/gitpod.yml/Dockerfile

--- index.js
+++ index.js
@@ -11,13 +11,6 @@ app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(require('./routes'));
 
-
-//  app.use(express.json());
-//  app.use(express.urlencoded({ extended: true }));
-//  app.use(morgan('dev'));
-
-// app.use(require('./routes'));
-
 app.listen(port, function(err){
     console.log('Listening on port: ' + port);
 });
---
+++ routes.js
@@ -0,0 +1,13 @@
+const express = require('express'),
+router = express.Router();
+
+var itemCtrl = require('./item-controller');
+
+router.get('/hello', itemCtrl.getWorld);
+
+router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
+
+router.post('/hello', itemCtrl.postWorld);
+
+
+module.exports = router;
