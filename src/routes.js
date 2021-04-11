4109198ab5c855c5fd6cdd7b89e2b62e59e81483
dgutierres26 <diegogutierres@hotmail.com>
2021-04-11 15:02:26 +0000

    setting the routes

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,23 @@
+const express = require('express'),
+router = express.Router();
+
+router.get('/hello/:foo/:bar', (req, res) => {
+  res.json({message: 'Hello BScBest!', data: [
+    req.params.foo,
+    req.params.bar
+]});
+
+});
+
+router.post('/hello', (req, res) => {
+    res.json({result: 'Post was sent', data: req.body});
+});
+
+
+
+
+
+module.exports = router;
 // var routes = require('express').Router();
 
 // routes.get('/', (req, resp) => {
