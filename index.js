0e76a17de40ec2ba988c7788930b8029f3a77cf4
dgutierres26 <diegogutierres@hotmail.com>
2021-04-11 17:33:49 +0000

    create Database conection

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require ('express'),
 morgan = require ('morgan'),
 cors = require('cors'),
 axios = require('axios'),
-bodyParser = require('body-parser');
+bodyParser = require('body-parser'),
+mongoose = require('mongoose');
 
 var app = express();
 var port = 8000;
@@ -11,6 +12,13 @@ app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(require('./routes'));
 
+
 app.listen(port, function(err){
     console.log('Listening on port: ' + port);
 });
+
+const dbURI = "mongodb://localhost/test";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then((result) => console.log('MongoDB is successfully connected'))
+        .catch((err) => console.log(err));
